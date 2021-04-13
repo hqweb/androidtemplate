fd4a16d6535a829f9d1c79374bc5e10d9f7f86cc
GitHub Action <action@github.com>
2021-04-13 06:00:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/hqweb/androidtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/hqweb/androidtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.hqweb.androidtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hqweb.androidtemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/hqweb/androidtemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hqweb/androidtemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hqweb.androidtemplate.services
 
+import com.github.hqweb.androidtemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
